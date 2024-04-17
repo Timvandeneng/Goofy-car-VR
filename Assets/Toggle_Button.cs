11e3e5fbb535c56b7ccf9dde1d2c33279cdd5e65
Timvandeneng <t.van.den.eng@sintlucasedu.nl>
2024-04-17 12:27:43 +0200

    added tags and some buttons

---
+++ Assets/Toggle_Button.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle_Button : MonoBehaviour
+{
+
+    [SerializeField] private Transform Model;
+    [SerializeField] private Vector3 restingpos;
+    [SerializeField] private Vector3 pushedpos;
+    public bool toggled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggled)
+        {
+            Model.localPosition = pushedpos;
+        }
+        else
+        {
+            Model.localPosition = restingpos;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            toggled = !toggled;
+        }
+    }
+}
