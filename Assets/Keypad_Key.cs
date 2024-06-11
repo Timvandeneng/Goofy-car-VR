4eae1e08719ad45e45fcab99cf522554bdb82c02
Qwerzer23 <127936920+JaredGrdsSL@users.noreply.github.com>
2024-06-11 16:32:56 +0200

    created first test module

--- Assets/Keypad_Key.cs
+++ Assets/Keypad_Key.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(ModuleInput))]
 public class Keypad_Key : MonoBehaviour
 {
     [SerializeField] private KeypadManager manager;
--- Assets/Scripts/Jared/ModuleSpawner.cs
+++ Assets/Scripts/Jared/ModuleSpawner.cs
@@ -4,40 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ModuleSpawner : MonoBehaviour {
-    [SerializeField] private GameObject modulePool;
+    public List<GameObject> moduleSpawners = new List<GameObject>();
 
-    [SerializeField] private List<GameObject> moduleSpawners = new List<GameObject>();
-
-    [SerializeField] private List<GameObject> AccelerateModules = new List<GameObject>();
-    [SerializeField] private List<GameObject> TurnModules = new List<GameObject>();
-    [SerializeField] private List<GameObject> BrakeModules = new List<GameObject>();
-    [SerializeField] private List<GameObject> BreakdownModules = new List<GameObject>();
-    [SerializeField] private List<GameObject> DummyModules = new List<GameObject>();
+    public List<GameObject> AccelerateModules = new List<GameObject>();
+    public List<GameObject> TurnModules = new List<GameObject>();
+    public List<GameObject> BrakeModules = new List<GameObject>();
+    public List<GameObject> BreakdownModules = new List<GameObject>();
+    public List<GameObject> DummyModules = new List<GameObject>();
 
     private List<int> moduleSelectionBag = new List<int>(); // Similar to a tetris bag, where you load all the numbers at once and shuffle so that you don't get any duplicates
 
     private List<GameObject> dummiesUsed = new List<GameObject>(); // A list used for the spawning of dummies
     private void Awake()
     {
-        RandomizeModules();
-    }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.R)) {
-            RandomizeModules();
-            Debug.Log("Hit key R");
-        }
-    }
-
-    public void RandomizeModules()
-    {
         for(int i = 0; i < moduleSpawners.Count; i++) { // Populating the list with numbers from 1 to the amount of module spawners
             moduleSelectionBag.Add(i + 1);
-
-            if(moduleSpawners[i].transform.childCount > 1) { // Removing all children from the modulespawners before placing in the new set
-                moduleSpawners[i].transform.GetChild(0).SetParent(modulePool.transform);
-            }
         }
 
         for(int i = 0; i < moduleSelectionBag.Count; i++) { // Shuffling the bag so the numbers are in a random order
